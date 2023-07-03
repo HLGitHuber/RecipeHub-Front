2ef37d46dbadb915fe15a9f95a893a4208001e50
HLGitHuber <hubertlewandowski2002@gmail.com>
2023-07-03 18:13:16 +0200

    working "search as you type"

--- recipe-hub-front/src/Search/Search.js
+++ recipe-hub-front/src/Search/Search.js
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useLocation } from 'react-router-dom';
 import SearchBar from './Searchbar';
 
-const Search = () => (
-  <div>
-    <h1>Search</h1>
-    <SearchBar />
-  </div>
+const Items = [
+    { id: '1', name: 'Apple' },
+    { id: '2', name: 'Milk' },
+    { id: '3', name: 'Bread' },
+    { id: '4', name: 'Chicken' },
+];
 
-)
+const doSearch = (term, query) => {
+    if(!term)
+        return Items
+
+    return Items.filter(
+        (item) => item.name.toLowerCase().includes(query))
+
+}
+
+const Search = () => {
+    
+    const { search } = window.location
+    const query = new URLSearchParams(search).get('q')
+    const [searchQuery, setSearchQuery] = useState(query || '')
+    const returned = doSearch(Items, searchQuery)
+    console.log(query)
+    return (
+
+    <div>
+        <h1>Search</h1>
+        <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+        <ul>
+            {returned.map(t => (
+                <li key={t.id}>{t.name}</li>
+            ))}
+        </ul>
+    </div>
+
+)}
 
 export default Search;
--- recipe-hub-front/src/Search/Searchbar.js
+++ recipe-hub-front/src/Search/Searchbar.js
@@ -1,20 +1,26 @@
-import React, {/*{Component}*/} from 'react';
 import '../css/SearchBar.css';
 
-const SearchBar = () => (
-  <form action="/" method="get">
-    <label htmlFor='header-search'>
-        <span className='visually-hidden'>Search recipes</span>
-    </label>
-    <input 
-        type="text"
-        id="header-search"
-        placeholder="Search blog posts"
-        name="s" 
-    />
-    <button type="submit">Search</button>
-  </form>
+const SearchBar = ({ searchQuery, setSearchQuery }) => {
+
+    return (
+
+        <form action='/search' method='get'>
+            <div>
+                <label htmlFor='header-search'>
+                    <span className='visually-hidden'>Search products</span>
+                </label>
+                <input 
+                    value={searchQuery}
+                    onInput={e => setSearchQuery(e.target.value)}
+                    type="text"
+                    id="header-search"
+                    placeholder="Search products"
+                    name="q" 
+                />
+            </div>
+        </form>
 
 )
+}
 
 export default SearchBar;
