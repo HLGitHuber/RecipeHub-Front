537765e02d120a80fff7f3cdec43bdec00fac076
arkadiuszwoznica <arkadiusz.piotr.woznica@gmail.com>
2023-07-10 12:12:45 +0200

    added fav button without final function

--- recipe-hub-front/src/RecipePage/RecipePage.js
+++ recipe-hub-front/src/RecipePage/RecipePage.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import "../css/basicPage.css"
+import Checkbox from '@mui/material/Checkbox';
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
+import Favorite from '@mui/icons-material/Favorite';
+import "../css/basicPage.css";
 
 const RecipePage = () => {
 
   const location = useLocation();
   const { state } = location;
+  const [checked, setChecked] = React.useState(true);
+
+  // const handleChange = (event) => {
+  //   setChecked(event.target.checked);
+  // };
+
+  const handleChange = () => {alert('changed')}
 
   const DisplayRecipe = () => {
     return (
       <div>
         <h2 className='heading'>{state.name}</h2> 
+        <div>
+          <Checkbox onChange={handleChange}
+            inputProps={{ 'aria-label': 'controlled' }} 
+            icon={<FavoriteBorder />} 
+            checkedIcon={<Favorite />} />
+        </div>
         <p className='text'>Time to prepare: {state.time}</p>
         <p className='text'>{state.text}</p>
         <p className='text'>Description:</p>
@@ -19,7 +35,7 @@ const RecipePage = () => {
     )
 }
 
-//adding fav button 
+
 
 return (
   <div className='background'>
