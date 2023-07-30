3158e195fbc6adbf97c5340f8e6d7fc5c7b902fb
arkadiuszwoznica <arkadiusz.piotr.woznica@gmail.com>
2023-07-30 09:31:53 +0200

    beautified front side

--- recipe-hub-front/src/Router/Homepage.js
+++ recipe-hub-front/src/Router/Homepage.js
@@ -11,22 +11,35 @@ function Homepage() {
   return (
     <div className='background'>
       <div className='container'>
-        <Link to={'/login'}>
-          <Button variant="contained" sx={{ backgroundColor: orange[500], fontSize: 15}}>Login</Button>
-        </Link>
+        <div className="loginButton">        
+          <Link to={'/login'}>
+            <Button variant="contained" sx={{ backgroundColor: orange[500], fontSize: 15}}>Login</Button>
+          </Link>
+        </div>
+
 
         <h1 className="heading">Homepage</h1>
         <h3 className="text">With us you're gonna find your dream meal Choose meal type</h3>
 
         <table>
-          <th><td>Snack - less than 250 kcal</td><td>Normal meal - more than 250 kcal</td></th>
-          <tr><td><Link to={'/search'}>
-              <CookieIcon sx={{ color: orange[500], fontSize: 80}}></CookieIcon>
-            </Link></td>
-            <td><Link to={'/search'}>
-              <DinnerDiningIcon sx={{ color: orange[500], fontSize: 80}}></DinnerDiningIcon>
-            </Link></td></tr>
+            <tr>
+              <td>Snack - less than 250 kcal</td>
+              <td>Normal meal - more than 250 kcal</td>
+            </tr>
+            <tr>
+              <td>
+                <Link to={'/search'}>
+                  <CookieIcon sx={{ color: orange[500], fontSize: 80}}></CookieIcon>
+                </Link>
+              </td>
+              <td>
+                <Link to={'/search'}>
+                  <DinnerDiningIcon sx={{ color: orange[500], fontSize: 80}}></DinnerDiningIcon>
+                </Link>
+              </td>
+            </tr>
         </table>
+
       </div>
   </div>
   );
