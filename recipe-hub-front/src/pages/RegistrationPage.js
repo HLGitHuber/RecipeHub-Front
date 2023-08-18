d842618ff4d7c4b0122aed5dd6959cb393e933e9
arkadiuszwoznica <arkadiusz.piotr.woznica@gmail.com>
2023-08-18 12:20:41 +0200

    register page base added

---
+++ recipe-hub-front/src/pages/RegistrationPage.js
@@ -0,0 +1,14 @@
+import "../css/basicPage.css";
+
+const RegistrationPage = () => {
+    return(
+        <div className='background'>
+            <div className='container'>
+                register here
+            </div>
+        </div>
+  
+    )
+}
+
+export default RegistrationPage
