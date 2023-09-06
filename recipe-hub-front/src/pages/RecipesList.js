5e99b3f337d98f7bc29a3b72fd508f0bfeaef8a4
arkadiuszwoznica <arkadiusz.piotr.woznica@gmail.com>
2023-09-06 13:37:33 +0200

    selects added

--- recipe-hub-front/src/pages/RecipesList.js
+++ recipe-hub-front/src/pages/RecipesList.js
@@ -1,27 +1,29 @@
-import React, { useState, useEffect}  from 'react'
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
-import "../css/basicPage.css"
-import apiSettings from '../config/apisettings.js'
-
+import "../css/basicPage.css";
+import apiSettings from '../config/apisettings.js';
 
 function RecipesList() {
   const location = useLocation();
   const [recipes, setRecipes] = useState([]);
+  const [selectedPreparationTime, setSelectedPreparationTime] = useState('');
+  const [selectedCalories, setSelectedCalories] = useState('');
+  const [filteredRecipes, setFilteredRecipes] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   const selectedProducts = location.state.selectedProducts;
 
   useEffect(() => {
     if (!selectedProducts || selectedProducts.length === 0) {
-      setIsLoading(false); 
-      return; 
+      setIsLoading(true);
+      return;
     }
-  
+
     const ingredientIDs = selectedProducts.map((product) => `ingredientIDs=${product.id}`).join('&');
     const apiUrl = `${apiSettings.apiUrlRecipe}recipesbyingredients?${ingredientIDs}`;
-  
+
     async function fetchRecipes() {
       try {
         const response = await fetch(apiUrl);
@@ -32,12 +34,33 @@ function RecipesList() {
         console.error('Error fetching recipes:', error);
       }
     }
-  
+
     fetchRecipes();
   }, [selectedProducts]);
 
+  const filterRecipes = () => {
+    return recipes.filter((recipe) => {
+      const prepTime = recipe.preparationTimeMin; 
+      const calories = recipe.calories;
+
+      if (selectedPreparationTime === "<20") {
+        return prepTime < 20;
+      } else if (selectedPreparationTime === "20-40") {
+        return prepTime >= 20 && prepTime <= 40;
+      } else if (selectedPreparationTime === ">40") {
+        return prepTime > 40;
+      }
+
+      return true; 
+    });
+  };
+
+  useEffect(() => {
+    const filteredRecipes = filterRecipes();
+    setFilteredRecipes(filteredRecipes);
+  }, [selectedPreparationTime]);
+
   const DisplayRecipe = (recipe) => {
-    console.log(recipe)
     return (
       <tr key={recipe.id}>
         <td>{recipe.name}</td>
@@ -54,19 +77,42 @@ function RecipesList() {
     );
   };
 
-
-  const displayAllRecipes = () => {
-    if (!recipes || recipes.length === 0 ) {
+  const displayAllRecipes = (recipeList) => {
+    if (!recipeList || recipeList.length === 0) {
       return <p>No recipes found. It's possible that you didn't choose any ingredients.</p>;
     }
-  
-    return recipes.map((recipe) => DisplayRecipe(recipe));
+
+    return recipeList.map((recipe) => DisplayRecipe(recipe));
   };
 
   return (
     <div className='background'>
       <div className='container'>
         <h2 className='heading'>Recipes we've found for you</h2>
+        <div className="filters">
+          <label>Preparation Time:</label>
+          <select
+            value={selectedPreparationTime}
+            onChange={(e) => setSelectedPreparationTime(e.target.value)}
+          >
+            <option value="">Select</option>
+            <option value="<20">&lt;20 min</option>
+            <option value="20-40">20-40 min</option>
+            <option value=">40">&gt;40 min</option>
+          </select>
+
+          <label>Calories:</label>
+          <select
+            value={selectedCalories}
+            onChange={(e) => setSelectedCalories(e.target.value)}
+          >
+            <option value="">Select</option>
+            <option value="<100">&lt;100</option>
+            <option value="100-300">100-300</option>
+            <option value=">300">&gt;300</option>
+          </select>
+        </div>
+
         {isLoading ? (
           <p>Loading...</p>
         ) : (
@@ -79,7 +125,7 @@ function RecipesList() {
                 <th></th>
               </tr>
             </thead>
-            <tbody>{displayAllRecipes()}</tbody>
+            <tbody>{displayAllRecipes(filteredRecipes)}</tbody>
           </table>
         )}
       </div>
