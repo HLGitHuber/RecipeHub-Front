2f8324cbc04bf5cafad398f3f59a6f308476bdc8
HLGitHuber <hubertlewandowski2002@gmail.com>
2023-09-19 22:59:06 +0200

    BIGRefactor: changed html elements to MaterialUI components

--- recipe-hub-front/src/components/RegistrationForm.js
+++ recipe-hub-front/src/components/RegistrationForm.js
@@ -3,9 +3,20 @@ import "../css/registrationForm.css"
 import axios from 'axios';
 import apiSettings from '../config/apisettings';
 import { useNavigate } from 'react-router-dom';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 const RegistrationForm = () => {
   const navigate = useNavigate();
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
   const [formData, setFormData] = useState({
     UserName: '',
@@ -16,13 +27,27 @@ const RegistrationForm = () => {
 
   const [errors, setErrors] = useState({
     UserName: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
+    Email: '',
+    Password: '',
+    ConfirmPassword: '',
   });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
+    const newErrors = { ...errors };
+
+    if (name === 'Email') {
+      const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+      if (!emailRegex.test(value)) {
+        newErrors[name] = 'Invalid email address.';
+      } else {
+        newErrors[name] = ''; // Clear the error if it's a valid email
+      }
+    } else {
+      newErrors[name] = ''; // Clear errors for other fields
+    }
+
+    setErrors(newErrors);
     setFormData((prevData) => ({
       ...prevData,
       [name]: value,
@@ -30,42 +55,25 @@ const RegistrationForm = () => {
   };
 
   const passwordCheck = (givenPassword) => {
-      const hasUpperCase = /[A-Z]/.test(givenPassword);
-      const hasLowerCase = /[a-z]/.test(givenPassword);
-      const hasNumber = /[0-9]/.test(givenPassword);
-      const hasSpecialChar = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/.test(givenPassword);
-      const isLengthValid = givenPassword.length >= 8;
-  
-      const isPasswordValid =
-        hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar && isLengthValid;
-  
-      return isPasswordValid;
+    const hasUpperCase = /[A-Z]/.test(givenPassword);
+    const hasLowerCase = /[a-z]/.test(givenPassword);
+    const hasNumber = /[0-9]/.test(givenPassword);
+    const hasSpecialChar = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/.test(givenPassword);
+    const isLengthValid = givenPassword.length >= 8;
+
+    const isPasswordValid =
+      hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar && isLengthValid;
+
+    return isPasswordValid;
   }
- 
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const newErrors = {};
-    if (!formData.UserName) {
-      newErrors.name = 'Name is required.';
-    }
-    if (!formData.Email) {
-      newErrors.email = 'Email is required.';
-    }
-    if (!formData.Password) {
-      newErrors.password = 'Password is required.';
-    }
-    if (!passwordCheck(formData.Password)) {
-      newErrors.password = 'Password has to have 8 characters including small and big letters, number and special character.';
-    }
-    if (!formData.ConfirmPassword) {
-      newErrors.confirmPassword = 'Confirm Password is required.';
-    }
-    if (formData.Password !== formData.ConfirmPassword) {
-      newErrors.confirmPassword = 'Passwords do not match.';
-    }
+    // Check if there are any errors in the current form data
+    const hasErrors = Object.values(errors).some((error) => !!error);
 
-    if (Object.keys(newErrors).length === 0) {
+    if (!hasErrors) {
       axios({
         method: 'options',
         url: apiSettings.apiUrlUserRegister,
@@ -82,8 +90,45 @@ const RegistrationForm = () => {
           console.log('Error', error);
         })
     } else {
-      setErrors(newErrors);
+      // You can choose to display a general form error here or handle it differently
+      console.log('Form has validation errors.');
     }
+  };
+
+  const handleBlur = (e) => {
+    const { name, value } = e.target;
+    const newErrors = { ...errors };
+
+    if (name === 'UserName' && !value) {
+      newErrors.UserName = 'Name is required.';
+    }
+
+    if (name === 'Email' && !value) {
+      newErrors.Email = 'Email is required.';
+    }
+
+    if (name === 'Password') {
+      if (!value) {
+        newErrors.Password = 'Password is required.';
+      } else if (!passwordCheck(value)) {
+        newErrors.Password =
+          'Password has to have 8 characters including small and big letters, number, and special character.';
+      } else {
+        newErrors.Password = ''; // Clear the error if it's a valid password
+      }
+    }
+
+    if (name === 'ConfirmPassword') {
+      if (!value) {
+        newErrors.ConfirmPassword = 'Confirm Password is required.';
+      } else if (formData.Password !== value) {
+        newErrors.ConfirmPassword = 'Passwords do not match.';
+      } else {
+        newErrors.ConfirmPassword = ''; // Clear the error if it's valid
+      }
+    }
+
+    setErrors(newErrors);
   };
 
   return (
@@ -91,48 +136,80 @@ const RegistrationForm = () => {
       <h2 className="registration-title">Registration Form</h2>
       <form className="registration-form" onSubmit={handleSubmit}>
         <div>
-        <span className="error">{errors.UserName}</span>
-          <input
+          <TextField
             className='input'
             type="text"
             name="UserName"
-            placeholder="Username"
-            sx={{ backgroundColor: 'bisque', ml: 1, flex: 1 }}
+            label="Username"
+            variant="outlined"
+            fullWidth
+            sx={{ marginBottom: 2 }} // Add any additional styling you need
             value={formData.UserName}
             onChange={handleChange}
+            onBlur={handleBlur}
+            error={!!errors.UserName}
+            helperText={errors.UserName || ' '}
           />
         </div>
         <div>
-        <span className="error">{errors.Email}</span>
-          <input
+          <TextField
             className='input'
             type="email"
             name="Email"
-            placeholder="Email"
+            label="Email"
+            variant="outlined"
+            fullWidth
+            sx={{ marginBottom: 2 }}
             value={formData.Email}
             onChange={handleChange}
+            onBlur={handleBlur}
+            error={!!errors.Email}
+            helperText={errors.Email || ' '}
           />
         </div>
         <div>
-        <span className="error">{errors.Password}</span>
-          <input
+          <TextField
             className='input'
-            type="password"
+            type={showPassword ? 'text' : 'password'}
             name="Password"
-            placeholder="Password"
+            label="Password"
+            variant="outlined"
+            fullWidth
+            sx={{ marginBottom: 2 }}
             value={formData.Password}
             onChange={handleChange}
+            onBlur={handleBlur}
+            error={!!errors.Password}
+            helperText={errors.Password || ' '}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
           />
         </div>
         <div>
-        <span className="error">{errors.ConfirmPassword}</span>
-          <input
+          <TextField
             className='input'
-            type="password"
+            type='password'
             name="ConfirmPassword"
-            placeholder="Confirm password"
+            label="Confirm Password"
+            variant="outlined"
+            fullWidth
+            sx={{ marginBottom: 2 }}
             value={formData.ConfirmPassword}
             onChange={handleChange}
+            onBlur={handleBlur}
+            error={!!errors.ConfirmPassword}
+            helperText={errors.ConfirmPassword || ' '}
           />
         </div>
         <button type="submit">Register</button>
