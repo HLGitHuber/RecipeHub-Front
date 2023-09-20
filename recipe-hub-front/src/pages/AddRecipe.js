7a1765c994cfed6a5e6bee6d4744950a8fb44335
HLGitHuber <hubertlewandowski2002@gmail.com>
2023-09-20 11:43:52 +0200

    feat: added home button

--- recipe-hub-front/src/pages/AddRecipe.js
+++ recipe-hub-front/src/pages/AddRecipe.js
@@ -5,6 +5,8 @@ import { Button } from '@mui/material';
 import { orange } from "@mui/material/colors";
 import React, { useState } from 'react';
 import Modal from '../components/LoginModal';
+import { Link } from 'react-router-dom';
+import HomeIcon from '@mui/icons-material/Home';
 
 const AddRecipePage = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -21,10 +23,14 @@ const AddRecipePage = () => {
         return (
             <div className='background'>
                 <div className='container'>
+                <Link to="/" className='home-link' style={{ top: 50, right: 15 }}>
+                    <HomeIcon className="home-icon" style={{ fontSize: 60, color: 'orange' }}/>
+                </Link>
                     <h1>You need to be logged in to create a new recipe!</h1>
                     <div className="loginButton">        
-          <Button variant="contained" sx={{ backgroundColor: orange[500], fontSize: 15}} onClick={openModal}>Login</Button>
-        </div><LoginModal/>
+                        <Button variant="contained" sx={{ backgroundColor: orange[500], fontSize: 15}} onClick={openModal}>Login</Button>
+                    </div>
+                    <LoginModal/>
                 </div>
                 <Modal isOpen={isModalOpen} onClose={closeModal} />
             </div>
@@ -34,6 +40,9 @@ const AddRecipePage = () => {
     return(
         <div className='background'>
             <div className='container'>
+                <Link to="/" className='home-link'>
+                    <HomeIcon className="home-icon" style={{ fontSize: 60, color: 'orange'}}/>
+                </Link>
                 <RecipeForm/>
             </div>
         </div>
